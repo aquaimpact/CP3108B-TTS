26eca6ddb8ef1de6a475f58ba2ac33e36599549c
aquaimpact <oweien12@gmail.com>
2025-06-11 12:18:47 +0800

    Add support to create WAV files based on user's text input

---
+++ ttsMain.py
@@ -0,0 +1,23 @@
+from ttsCore import *
+
+def main():
+    print("Google Cloud Text-to-Speech API Test")
+    print(("-" * 60) + "\n")
+    
+    print("Listing languages:")
+    list_languages()
+    
+    language = input("\nSelect the language you want to use the voice for: ")
+
+    print(f"\nListing voices for {language}:")
+    list_voices(language_code=language)
+    
+    voice_name = input("\nSelect the voice you want to use: ")
+    text = input("Enter the text you want to convert to speech: ");
+
+    print(f"\nGenerating speech for voice '{voice_name}' with text: {text}...")
+    text_to_wav(voice_name, text)
+    
+
+if __name__ == "__main__":
+    main()
